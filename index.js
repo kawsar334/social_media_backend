17c41b7cbd482380943f73c12d39da21fc101156
kawsar firoz <kawsarfiroz11@gmail.com>
2024-10-13 10:19:12 +0300

    updted

--- index.js
+++ index.js
@@ -5,16 +5,13 @@ const cookieParser = require("cookie-parser");
 const cors = require("cors");
 const connect = require("./db");
 const Post = require("./models/Post")
-// const authRoute = require("./routes/auth");
-// const userRoute = require("./routes/user");
-// const postRoute = require("./routes/post");
-// const commentRoute = require("./routes/comment");
 
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors()); // Enable CORS for all routes
+app.use(cors()); 
+
 
 // Add a simple GET request
 app.get("/", async(req, res) => {
@@ -39,18 +36,6 @@ app.get("/", async(req, res) => {
 
 });
 
-// Global error handling middleware
-// app.use((err, req, res, next) => {
-//     const message = err.message || "Something went wrong";
-//     const status = err.status || 500;
-//     return res.status(status).json({
-//         message,
-//         status,
-//         success: false,
-//     });
-// });
-
-// Connect to the database
 connect();
 
 app.listen(PORT, () => {
