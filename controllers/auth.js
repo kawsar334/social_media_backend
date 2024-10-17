6b39d7646607449dff77d9f9bd22d0fe4b19e70f
kawsar firoz <kawsarfiroz11@gmail.com>
2024-10-17 14:46:29 +0300

    updated

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,53 +1,48 @@
-
-
-
 const User = require("../models/User");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs"); // Use bcryptjs
 const jwt = require("jsonwebtoken");
-const nodemailer = require("nodemailer")
 
-
-// register user 
-const Register = async(req, res,next)=>{
-    try{
-        const userExist = await User.findOne({email:req.body.email});
-        if(userExist){
-            return res.status(200).json({message:"User already Exist", success:false});
-        }else{
-            const hashedPassword = await bcrypt.hash(req.body.password, 10); 
+// Register user 
+const Register = async (req, res, next) => {
+    try {
+        const userExist = await User.findOne({ email: req.body.email });
+        if (userExist) {
+            return res.status(400).json({ message: "User already exists", success: false });
+        } else {
+            const hashedPassword = await bcrypt.hash(req.body.password, 10);
             const user = new User({
-                ...req.body, 
+                ...req.body,
                 password: hashedPassword,
             });
 
             await user.save();
-            res.status(200).json({message:"User Registered Successfully", success:true});
+            res.status(201).json({ message: "User registered successfully", success: true });
         }
-    }catch(err){
+    } catch (err) {
         next(err);
     }
 }
 
-// login user
-const Login = async(req, res,next)=>{
-    try{
-        const user = await User.findOne({email:req.body.email});
-        if(!user){
-            return res.status(200).json({message:"User not found", success:false});
+// Login user
+const Login = async (req, res, next) => {
+    try {
+        const user = await User.findOne({ email: req.body.email });
+        if (!user) {
+            return res.status(404).json({ message: "User not found", success: false });
         }
-        const hashedPassword = await bcrypt.compare(req.body.password, user.password);
-        if(!hashedPassword){
-            return res.status(200).json({message:"Invalid password", success:false});
+        const isMatch = await bcrypt.compare(req.body.password, user.password);
+        if (!isMatch) {
+            return res.status(401).json({ message: "Invalid password", success: false });
         }
-        const token = jwt.sign({ id: user._id }, process.env.SECRETE, { expiresIn: '1d' });
+        const token = jwt.sign({ id: user._id }, process.env.SECRET, { expiresIn: '1d' });
         res.cookie('token', token, {
-            httpOnly: true, 
-            maxAge: 24 * 60 * 60 * 1000 
+            httpOnly: true,
+            maxAge: 24 * 60 * 60 * 1000 // 1 day
         });
-        res.status(200).json({message:"User logged in successfully", success:true, token});
-    }catch(err){
+        res.status(200).json({ message: "User logged in successfully", success: true, token });
+    } catch (err) {
         next(err);
     }
 }
 
-module.exports = { Register, Login };  
+module.exports = { Register, Login };
