06130123c3b97d6a352ad38c4365fa7376626b21
kawsar firoz <kawsarfiroz11@gmail.com>
2024-10-19 17:27:11 +0300

    updated in route file of comment

--- routes/comment.js
+++ routes/comment.js
@@ -8,13 +8,13 @@ const { verifyToken } = require("../jsonwebToken")///------------req.user.id
 // create comment
 router.post("/addcomment/:postId", verifyToken,addComment)
 // update comment
-router.put("/addcomment/:id", verifyToken, updateComment);
+router.put("/:id", verifyToken, updateComment);
 
 // delete comment
 router.delete("/:id", verifyToken, deleteComment);
 
 // get comments
-router.post("/:postId", getComments);
+router.get("/:postId", getComments);
 
 
 
